783da1c30dce7525a889837b17f000b766f71ad2
Gianluca18092004 <gianluca18092004@gmail.com>
2022-06-18 01:36:49 -0300

    Add WebSite

---
+++ index.js
@@ -0,0 +1,4 @@
+var downloadIE55 = function (form) {
+    window.location.href = "/".concat(form.lang.value, "/ie5setup.exe");
+    return false;
+};
---
+++ index.ts
@@ -0,0 +1,4 @@
+const downloadIE55 = (form) => {
+    window.location.href=`/${form.lang.value}/ie5setup.exe`;
+    return false;
+};
