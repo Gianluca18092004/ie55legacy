b216d7c4408e4caf9198fbbf19b0cb7233e02bb4
Gianluca18092004 <55612736+Gianluca18092004@users.noreply.github.com>
2022-06-19 17:33:24 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 var downloadIE55 = function (form) {
-    window.location.href = "/".concat(form.lang.value, "/IE5SETUP.EXE");
+    window.location.href = "/" + form.lang.value + "/IE5SETUP.EXE";
     return false;
 };
